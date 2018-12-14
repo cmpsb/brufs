7f871376ac491c24bc8437c5d4a50d672a89243d
Luc Everse <luc@wukl.net>
2018-12-14 09:18:10 +0100

    Document Root.hpp

--- libbrufs/include/Root.hpp
+++ libbrufs/include/Root.hpp
@@ -38,6 +38,9 @@ class Inode;
 class File;
 class Directory;
 
+/**
+ * A B+tree containing inodes belonging to a root.
+ */
 class InoTree : public BmTree::BmTree<InodeId, InodeHeader> {
 private:
     Root &owner;
@@ -50,59 +53,201 @@ public:
     Status on_root_change(Address new_addr) override;
 };
 
+/**
+ * A root in the filesystem.
+ */
 class Root {
+    /**
+     * The filesystem the root belongs to.
+     */
     Brufs &fs;
 
+    /**
+     * The on-disk structure describing the header.
+     */
     RootHeader header;
 
+    /**
+     * The main inode tree.
+     */
     InoTree it;
+
+    /**
+     * The alternate inode tree, containing extended attributes for example.
+     */
     InoTree ait;
 
     friend InoTree;
 
+    /**
+     * Enables or disables automatic storage upon modification.
+     */
     bool enable_store = true;
 
 public:
+    /**
+     * Constructs a new root handle.
+     * 
+     * @param fs the filesystem the root is on
+     * @param hdr the on-disk header of the root
+     */
     Root(Brufs &fs, const RootHeader &hdr);
 
     // Roots are non-copyable
     Root(const Root &other) = delete;
     Root &operator=(const Root &other) = delete;
 
+    /**
+     * Returns the on-disk structure representing the header.
+     * 
+     * @return the on-disk structure
+     */
     const RootHeader &get_header() { return this->header; }
+    const RootHeader &get_header() const { return this->header; }
 
+    /**
+     * Returns the filesystem the root is on.
+     * 
+     * @return the filesystem
+     */
     Brufs &get_fs() { return this->fs; }
+    const Brufs &get_fs() const { return this->fs; }
 
-    const RootHeader &get_header() const {
-        return this->header;
-    }
-
+    /**
+     * Initializes the root.
+     * 
+     * @param ihb an inode header builder used to construct the actual root directory
+     * 
+     * @return the status return code
+     */
     Status init(const InodeHeaderBuilder &ihb = InodeHeaderBuilder());
+
+    /**
+     * Writes the root to disk.
+     * 
+     * @return the status return code
+     */
     Status store();
 
+    /**
+     * Creates a temporary inode header of the correct size.
+     * 
+     * @return a heap-allocated header
+     */
     InodeHeader *create_inode_header() const;
+
+    /**
+     * Destroys an allocated inode header.
+     * 
+     * @param header the header to destroy
+     */
     void destroy_inode_header(InodeHeader *header) const;
 
+    /**
+     * Inserts an inode into the root.
+     * 
+     * @param id the inode ID of the inode to add
+     * @param ino the inode to add
+     * 
+     * @return the status return code
+     */
     Status insert_inode(const InodeId &id, const InodeHeader *ino);
-    Status find_inode(const InodeId &id, InodeHeader *ino);
-    Status update_inode(const InodeId &id, const InodeHeader *ino);
-    Status remove_inode(const InodeId &id, InodeHeader *ino);
 
+    /**
+     * Looks up an inode in the root.
+     * 
+     * @param id the inode ID of the inode to look for
+     * @param ino where to store the found inode header
+     * 
+     * @return the status return code
+     */
+    Status find_inode(const InodeId &id, InodeHeader *ino);
+
+    /**
+     * Updates an inode in the root.
+     * 
+     * @param id the inode ID of the inode to update
+     * @param ino the new contents of the inode
+     * 
+     * @return the status return code
+     */
+    Status update_inode(const InodeId &id, const InodeHeader *ino);
+
+    /**
+     * Removes an inode from the root.
+     * 
+     * @param id the ID of the inode to remove
+     * @param ino where to store the header of the removed inode
+     * 
+     * @return the status return code
+     */
+    Status remove_inode(const InodeId &id, InodeHeader *ino = nullptr);
+
+    /**
+     * Opens an inode by its ID.
+     * 
+     * @param id the ID of the inode to open
+     * @param inode the inode handle to initalize with the inode
+     * 
+     * @return the status return code
+     */
     Status open_inode(const InodeId &id, Inode &inode);
+
+    /**
+     * Opens a file by its inode ID.
+     * 
+     * @param id the inode ID of the file to open
+     * @param file the file handle to initialize with the inode
+     * 
+     * @return the status return code
+     */
     Status open_file(const InodeId &id, File &file);
+
+    /**
+     * Opens a directory by its inode ID.
+     * 
+     * @param id the inode ID of the directory to open
+     * @param dir the directory handle to initialize with the inode
+     * 
+     * @return the status return code
+     */
     Status open_directory(const InodeId &id, Directory &dir);
 
+    /**
+     * Opens an inode by its path.
+     * 
+     * @param path the path to the inode
+     * @param inode the inode handle to initialize with the inode
+     * 
+     * @return the status return code
+     */
     Status open_inode(const Path &path, Inode &inode);
+
+    /**
+     * Opens a file by its path.
+     * 
+     * @param path the path to the file
+     * @param file the file handle to initialize with the inode
+     * 
+     * @return the status return code
+     */
     Status open_file(const Path &path, File &file);
+
+    /**
+     * Opens a directory by its path.
+     * 
+     * @param path the path to the directory
+     * @param dir the directory handle to initialize with the inode
+     * 
+     * @return the status return code
+     */
     Status open_directory(const Path &path, Directory &dir);
 
-    operator const RootHeader &() const {
-        return this->header;
-    }
-
-    operator RootHeader &() {
-        return this->header;
-    }
+    /**
+     * Implicitly casts a root to its on-disk header.
+     */
+    operator const RootHeader &() const { return this->header; }
+    operator RootHeader &() { return this->header; }
 };
 
 inline Status InoTree::on_root_change(Address new_addr) {
